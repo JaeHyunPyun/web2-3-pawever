6f19c0b61e3d1e57093c3a945e54792a0c7bd34b
juyeon6069 <jynam6069@gmail.com>
2025-02-26 11:36:48 +0900

    refactor : 전체 후원 금액 조회 응답형태 수정(#15)

--- src/main/java/com/pawever/server/domain/donation/controller/DonationController.java
+++ src/main/java/com/pawever/server/domain/donation/controller/DonationController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -37,6 +38,8 @@ public class DonationController {
     @GetMapping("api/donations/amount")
     public ResponseEntity<ApiResponse> getTotalDonationAmount() {
         double totalDonationAmount = donationService.getTotalDonationAmount();
-        return ResponseEntity.ok(ApiResponse.success(ResponseCodeEnum.SUCCESS, totalDonationAmount));
+        Map<String, Object> responseData = new HashMap<>();
+        responseData.put("donorAmount", totalDonationAmount);
+        return ResponseEntity.ok(ApiResponse.success(ResponseCodeEnum.SUCCESS, responseData));
     }
 }
