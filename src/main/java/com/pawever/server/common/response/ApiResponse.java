4a59c27a622805ebc343009a1a1eb1a240063e27
juyeon6069 <jynam6069@gmail.com>
2025-02-26 14:51:26 +0900

    refactor : 응답에 message 추가(#15)

--- src/main/java/com/pawever/server/common/response/ApiResponse.java
+++ src/main/java/com/pawever/server/common/response/ApiResponse.java
@@ -10,10 +10,11 @@ public class ApiResponse {
     private Boolean isSuccess;
     private String status;
     private String code;
+    private String message;
     private Object data;
 
     public static ApiResponse success(ResponseCodeEnum responseCodeEnum, Object data){
-        return  new ApiResponse(true,responseCodeEnum.getStatus().name(),responseCodeEnum.getCode(),data);
+        return  new ApiResponse(true,responseCodeEnum.getStatus().name(),responseCodeEnum.getCode(),responseCodeEnum.getMessage(),data);
     }
     public static ApiResponse success(ResponseCodeEnum responseCodeEnum){
         return success(responseCodeEnum,null);
@@ -23,7 +24,7 @@ public class ApiResponse {
     }
 
     public static ApiResponse fail(ResponseCodeEnum responseCodeEnum, Object data){
-        return new ApiResponse(false,responseCodeEnum.getStatus().name(),responseCodeEnum.getCode(),data);
+        return new ApiResponse(false,responseCodeEnum.getStatus().name(),responseCodeEnum.getCode(),responseCodeEnum.getMessage(),data);
     }
 
 
