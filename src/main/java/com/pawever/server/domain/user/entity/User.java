7092f5012471a019825122dd5c11ee774e52c69a
jaehyunpyun <pyunjaehyun@gmail.com>
2025-02-27 16:46:27 +0900

    feat : UserEntity 프로토 타입 작성

--- src/main/java/com/pawever/server/domain/user/entity/User.java
+++ src/main/java/com/pawever/server/domain/user/entity/User.java
@@ -1,4 +1,76 @@
 package com.pawever.server.domain.user.entity;
 
+import com.pawever.server.domain.user.enums.Role;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+@Table(name = "user")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id", nullable = false)
+    private Long userId;  // 유저 id
+
+    @Column(name= "name", nullable = false, length=50)
+    private String name; // 유저 name
+
+    @Column(name= "email", nullable = false, length=255)
+    private String email; // 유저 email
+
+    @Column(name= "profile_image_url", length=255)
+    private String profileImageUrl; // 유저 profile image
+
+    @Column(name= "social_login_uuid", nullable = false, length=36)
+    private String socialLoginUuid; // 카카오, 구글 제공 uuid
+
+    @Column(name = "social_login_provider", nullable = false, length=10)
+    private String socialLoginProvider; // 카카오, 구글
+
+    @Setter
+    @Column(name = "latitude", nullable = true, precision = 10, scale = 7)
+    private BigDecimal latitude;  // 유저 회원가입시 위치(위도)
+
+    @Setter
+    @Column(name = "longitude", nullable = true, precision = 10, scale = 7)
+    private BigDecimal longitude;  // 유저 회원가입시 위치(경도)
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false, length = 10)
+    private Role role = Role.ROLE_USER; // 유저 권한
+
+    @CreatedDate
+    @Column(name="created_at", nullable= false, updatable = false)
+    private LocalDateTime created_at;   // 최초 생성 시각(자동 업데이트)
+
+    @LastModifiedDate
+    @Column(name="updated_at")
+    private LocalDateTime updated_at;  // 마지막 수정 시각(자동 업데이트)
+
+    @Column(name="deleted_at")
+    private LocalDateTime deleted_at; // 삭제시각(수동 업데이트)
+
+    @Column(name="is_deleted", nullable = false)
+    private Boolean isDeleted = false; // 삭제 여부
 }
