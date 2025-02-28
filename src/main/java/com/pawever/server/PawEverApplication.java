49bf98c2c2f9b0b8f717782bec5df46b0875f3bf
jaehyunpyun <pyunjaehyun@gmail.com>
2025-02-28 11:38:03 +0900

    fix : 스프링에서 RedisRepository를 인식할 수 있도록 어노테이션 추가

--- src/main/java/com/pawever/server/PawEverApplication.java
+++ src/main/java/com/pawever/server/PawEverApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
@@ -12,6 +13,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 @EnableScheduling //스케줄링 활성화
 @EnableConfigurationProperties(JwtProperties.class)
+@EnableRedisRepositories
 public class PawEverApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/pawever/server/domain/user/repository/redis/RefreshTokenRepository.java
+++ src/main/java/com/pawever/server/domain/user/repository/redis/RefreshTokenRepository.java
@@ -3,7 +3,9 @@ package com.pawever.server.domain.user.repository.redis;
 import com.pawever.server.domain.user.entity.redis.RefreshToken;
 import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
     Optional<RefreshToken> findRefreshTokenByRefreshToken(String refreshToken);
 }
