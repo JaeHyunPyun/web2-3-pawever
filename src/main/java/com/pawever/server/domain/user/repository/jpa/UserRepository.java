ef3f9f9dbb5e0295d58ebaadb7c57a150ffad9ee
jaehyunpyun <pyunjaehyun@gmail.com>
2025-03-01 22:22:29 +0900

    fix : UserRepository 스프링 빈으로 등록될 수 있도록 @Repository 어노테이션 추가

--- src/main/java/com/pawever/server/domain/user/repository/jpa/UserRepository.java
+++ src/main/java/com/pawever/server/domain/user/repository/jpa/UserRepository.java
@@ -4,7 +4,9 @@ import com.pawever.server.domain.user.entity.jpa.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     @Query(value = "SELECT u FROM User u WHERE u.socialLoginUuid = :social_login_uuid")
