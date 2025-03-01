194ccebc4c7be24967fd85d876a56e17a2b8eb63
jaehyunpyun <pyunjaehyun@gmail.com>
2025-03-01 22:19:47 +0900

    chore : 불필요한 테스트 코드 주석처리

--- src/test/java/com/pawever/server/domain/cicd/service/RedisServiceTest.java
+++ src/test/java/com/pawever/server/domain/cicd/service/RedisServiceTest.java
@@ -7,23 +7,23 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 public class RedisServiceTest {
-    // Mock 객체 없이 실제 Redis 테스트
-
-    @Autowired
-    private RedisService redisService;
-
-    @Test
-    public void testSaveAndGetValue() {
-        // given
-        String key = "testKey";
-        String value = "testValue";
-
-        // when
-        redisService.saveValue(key, value);
-        String retrievedValue = redisService.getValue(key);
-
-        // then
-        assertThat(retrievedValue).isEqualTo(value);
-        System.out.println("Redis Test without Mock success");
-    }
+//    // Mock 객체 없이 실제 Redis 테스트
+//
+//    @Autowired
+//    private RedisService redisService;
+//
+//    @Test
+//    public void testSaveAndGetValue() {
+//        // given
+//        String key = "testKey";
+//        String value = "testValue";
+//
+//        // when
+//        redisService.saveValue(key, value);
+//        String retrievedValue = redisService.getValue(key);
+//
+//        // then
+//        assertThat(retrievedValue).isEqualTo(value);
+//        System.out.println("Redis Test without Mock success");
+//    }
 }
