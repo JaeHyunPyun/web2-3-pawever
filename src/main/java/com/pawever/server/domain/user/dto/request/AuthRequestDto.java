8591528ec42e7d8ad3fe457b85027f1fb6d9fa18
jaehyunpyun <pyunjaehyun@gmail.com>
2025-03-05 03:16:34 +0900

    feat : 회원가입시 소셜로그인으로부터 profileImage를 받아오지 못하는 경우 디폴트 이미지 적용하도록 구현

--- src/main/java/com/pawever/server/domain/user/dto/request/AuthRequestDto.java
+++ src/main/java/com/pawever/server/domain/user/dto/request/AuthRequestDto.java
@@ -13,7 +13,9 @@ import lombok.NoArgsConstructor;
 public class AuthRequestDto {
     private String socialLoginUuid;         //소셜로그인 uuid
     private String name;                    //소셜로그인 닉네임
-    private String profileImageUrl;         //프로필 이미지
+    //프로필 이미지
+    @Builder.Default
+    private String profileImageUrl="https://pawever-user-s3.s3.ap-northeast-2.amazonaws.com/1f1b3200-91b9-49c9-b40d-765bd600c5f6pawever_user_default_image_2025_03_04_22_04.jpg";
     private String email;                   //소셜로그인 이메일
     private String socialLoginProvider;     //카카오 or 구글
     private BigDecimal latitude;            //사용자 위치 - 위도
