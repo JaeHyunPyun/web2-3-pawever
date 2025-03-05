2d5b1c841d28ebc101b09ca21623916fb478d0fd
jaehyunpyun <pyunjaehyun@gmail.com>
2025-03-05 11:43:37 +0900

    feat : 회원가입시 소셜 인증에서 profileImageUrl가 전달되지 않는 경우 디폴트 이미지로 회원가입하도록 설정

---
+++ src/main/java/com/pawever/server/domain/user/constant/UserConstants.java
@@ -0,0 +1,8 @@
+package com.pawever.server.domain.user.constant;
+
+public class UserConstants {
+    // user디폴트 이미지 주소
+    public static final String DEFAULT_PROFILE_IMAGE_URL =
+        "https://paweverbucket.s3.ap-northeast-2.amazonaws.com/9c2752b2-27c9-44c1-a203-9b426c043397pawever_user_default_image_2025_03_04_22_04.jpg";
+
+}
