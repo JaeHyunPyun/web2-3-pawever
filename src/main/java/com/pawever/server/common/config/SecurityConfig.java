fa88023ab6075357958f836007edc7c5f319eee4
jaehyunpyun <pyunjaehyun@gmail.com>
2025-03-06 14:38:20 +0900

    chore : SecurityConfig 클래스를 common 도메인으로 이동

--- src/main/java/com/pawever/server/common/config/SecurityConfig.java
+++ src/main/java/com/pawever/server/common/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.pawever.server.domain.user.config;
+package com.pawever.server.common.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.pawever.server.domain.user.handler.FilterExceptionHandler;
@@ -9,16 +9,12 @@ import com.pawever.server.domain.user.jwt.JwtUtil;
 import com.pawever.server.domain.user.service.AccessTokenService;
 import com.pawever.server.domain.user.service.RefreshTokenService;
 import com.pawever.server.domain.user.service.UserService;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
