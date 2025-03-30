fb13e423ac485c2eed3d537be343fc9c0eee93b2
jaehyunpyun <pyunjaehyun@gmail.com>
2025-03-31 00:08:06 +0900

    fix : GeoIp DB파일을 Jar에 포함시킬 경우, 랜덤액세스가 불가한 문제가 있어 임시파일 생성하도록 수정

--- src/main/java/com/pawever/server/common/infra/IpGeoLocationProvider.java
+++ src/main/java/com/pawever/server/common/infra/IpGeoLocationProvider.java
@@ -3,9 +3,12 @@ package com.pawever.server.common.infra;
 import com.maxmind.geoip2.DatabaseReader;
 import com.maxmind.geoip2.exception.GeoIp2Exception;
 import com.maxmind.geoip2.model.CityResponse;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.InetAddress;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
@@ -20,7 +23,14 @@ public class IpGeoLocationProvider {
         // 클래스패스에서 파일을 읽는다
         ClassPathResource classPathResource = new ClassPathResource("data/GeoLite2-City.mmdb");
         InputStream dbStream = classPathResource.getInputStream();
-        this.ipGeoDatabaseReader = new DatabaseReader.Builder(dbStream).build();
+
+        // 임시 파일 생성 (앱 종료 시 자동 삭제)
+        File tempFile = File.createTempFile("GeoLite2-City", ".mmdb");
+        tempFile.deleteOnExit();
+        Files.copy(dbStream, tempFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+
+        // 임시 파일을 DatabaseReader에 넘김
+        this.ipGeoDatabaseReader = new DatabaseReader.Builder(tempFile).build();
     }
 
     public CityResponse getGeoLocationByIp(InetAddress inetAddress) {
